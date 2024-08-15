14ceac9f2f786a1e1418903a257b9bd787d01c76
RuslanPol <vvrv@yandex.ru>
2024-08-15 13:15:00 +0700

    added func addNoteToList in app.js

--- front/public/app.js
+++ front/public/app.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('noteForm');
 const notesList = document.getElementById('notesList');
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const text = document.getElementById('noteText').value;
@@ -21,29 +22,40 @@ form.addEventListener('submit', async (event) => {
     notesList.appendChild(listItem);
     form.reset();
     location.reload();
-    await loadNotes();
+
 });
 
-async function loadNotes() {
-    const response = await fetch(`/api/notes`);
 
-    const notes = await response.json();
-    notes.forEach(note => {
-        const infoLink = document.createElement('a')
-        const listItem = document.createElement('li');
+ async function addNoteToList(note) {
+    const listItem = document.createElement('li');
+    const infoLink = document.createElement('a')
+     const br = document.createElement('br');
+    listItem.textContent = `СОДЕРЖАНИЕ: ${note.context}`
+    infoLink.textContent = `info`
+    infoLink.href = `note.html?id=${note.id}`;
+    console.log(infoLink.href);
+     listItem.appendChild(br);
+    listItem.appendChild(infoLink);
+    notesList.appendChild(listItem);
 
-        listItem.innerText =
-            `СОДЕРЖАНИЕ: ${note.context}\n`;
-        infoLink.textContent = `INFO`
-        infoLink.href = `note.html?id=${note.id}`;
-        console.log(infoLink.href);
-
-        listItem.appendChild(infoLink)
-        notesList.appendChild(listItem)
-    });
 
 }
 
-loadNotes().then(r => console.log(r));
+
+async function loadNotes() {
+    const response = await fetch('/api/notes');
+
+    if (response.ok) {
+        const notes = await response.json();
+        notesList.innerHTML = '';
+        notes.forEach(note => {
+             addNoteToList(note);
+        });
+    } else {
+        console.error('Ошибка при загрузке заметок:', response.statusText);
+    }
+}
+
+  loadNotes().then(r => r);
 
 
