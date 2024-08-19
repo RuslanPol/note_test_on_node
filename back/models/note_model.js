c45e9fa50ad7a14cf62f7e50289f2bcbcf81602d
RuslanPol <vvrv@yandex.ru>
2024-08-19 19:58:27 +0700

    factoring createNote

---
+++ back/models/note_model.js
@@ -0,0 +1,46 @@
+/*
+1. Комитишь и пушишь dev branch
+2. Переключаешься в master
+3. Кликаешь на dev --- > merge into master
+*/
+
+const { DataTypes } = require('sequelize')
+const db = require('../db')
+
+const Notes = db.define('notes',
+    // Описание таблиц
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status_note: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            // type: DataTypes.TIME,
+            allowNull: false
+        },
+
+    },
+    // Опции
+    {
+
+        timestamps: false
+
+
+    }
+)
+
+
+
+
+module.exports = Notes
